39539859570d737ff44f0655f921d0768b7b70de
UdYnamite <89peterj89@gmail.com>
2023-07-20 19:53:18 +0700

    Created toggle drawing feature Initial state is off and you need to turn on to draw. Created toggle button and toggle on click event listener on grid area.

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 const gridsContainer = document.querySelector('#grid-container');
 const sizeButton = document.querySelector('#size-btn');
-
+const toggleButton = document.querySelector('.toggle-btn');
 let gridSize = 16;
+let enabled = false;
+generateGrid(gridSize);
+const cells = document.querySelectorAll('.cell');
+
+gridsContainer.addEventListener('click', function() {
+    if (!enabled) {
+        toggleDrawing();
+        toggleButton.classList.add('btn-enabled')
+    }
+}, { once: true })
+
+toggleButton.addEventListener('click', function() {
+    toggleDrawing();
+})
+
+function toggleDrawing() {
+    if (enabled) {
+        cells.forEach(cell => cell.removeEventListener('mouseover', colorCell));
+        toggleButton.classList.remove('btn-enabled');
+        enabled = false
+    } else {
+        cells.forEach(cell => cell.addEventListener('mouseover', colorCell));
+        toggleButton.classList.add('btn-enabled');
+        enabled = true
+    }
+}
 
 sizeButton.addEventListener('click', () => {
     gridSize = prompt('Type grid size (max 100)');
+    if (gridSize > 100 || gridSize === null || gridSize === '') {
+        if (gridSize > 100) {
+            alert("More than 100 squares will explode your pc!")
+        }
+        return;
+    }
     while (gridsContainer.hasChildNodes()) {
         gridsContainer.removeChild(gridsContainer.firstChild);
     }
@@ -22,20 +54,16 @@ function generateGrid(size) {
         for (let j = 0; j < size; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.addEventListener('mouseover', colorCell);
             row.appendChild(cell);
         }
     }
+
 }
 
-const cells = document.querySelectorAll('.cell');
+
+
 
 function colorCell() {
     this.style.backgroundColor = 'black';
 }
 
-generateGrid(gridSize);
-
-
-
-cells.forEach(cell => cell.addEventListener('mouseover', color(cell)));
